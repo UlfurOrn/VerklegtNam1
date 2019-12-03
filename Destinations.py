5368b8f970776873a6f259fb15b86d9e7716d72b
Arnaldur <aarnaldur@gmail.com>
2019-12-03 07:17:09 +0000

    timing work started

--- Destinations.py
+++ Destinations.py
@@ -1,14 +1,14 @@
 import datetime
 
 class Destinations:
-    def __init__():
+    def __init__(self):
         self.destinations = []
 
-    def add_destination(country, airport, flight_time, distance, contact_id):
+    def add_destination(self, country, airport, flight_time, distance, contact_id):
         self.destinations.append(Destination(self.destinations.len(), country, airport, flight_time, distance, contact_id))
 
 class Destination:
-    def __init__(id, country, airport, flight_time, distance, contact_id):
+    def __init__(self, id, country, airport, flight_time, distance, contact_id):
         self.id = id
         self.country = country
         self.airport = airport
@@ -16,5 +16,5 @@ class Destination:
         self.distance = distance
         self.contact_id = contact_id
 
-    def __str__():
+    def __str__(self):
         return "destination: " + self.country + self.airport + self.flight_time + self.distance + self.contact_id
