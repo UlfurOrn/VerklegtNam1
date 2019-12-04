d2e145fc6ab0b7e2987812225d71304773b7dbad
Arnaldur <aarnaldur@gmail.com>
2019-12-04 15:50:44 +0000

    dictified everything

--- Destinations.py
+++ Destinations.py
@@ -2,40 +2,30 @@ from datetime import date
 import csv
 
 
-class Destination:
-    def __init__(self,
-                 airport="",
-                 country="",
-                 flightTime=date(1, 1, 1),
-                 distance=0,
-                 contactId=""):
-        self.airport = airport
-        self.country = country
-        self.flightTime = flightTime
-        self.distance = distance
-        self.contactId = contactId
-
-    def __str__(self):
-        return "Destination: " + ", " + self.airport + ", " + self.country + ", " + str(
-            self.flightTime) + ", " + str(
-                self.distance) + ", " + self.contactId
+class Destination(dict):
+    def nosdf(self):
+        pass
 
 
 class DestinationContainer:
     def __init__(self):
         self._destinations = {}
-        with open("../UPDATEDSTUDENTDATA/Destinations.csv") as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                newDestination = Destination(row[reader.fieldnames[0]],
-                                             row[reader.fieldnames[1]])
-                self._destinations[newDestination.airport] = newDestination
+        self.load_csv()
 
     def __str__(self):
         return "".join([
             str(k) + ": " + str(v) + "\n"
             for (k, v) in self._destinations.items()
         ])
+
+    def add(self, destination):
+        self._destinations["id"] = destination
+
+    def load_csv(self):
+        with open("../UPDATEDSTUDENTDATA/Destinations.csv") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                self.add(Destination(row))
 
 
 print(DestinationContainer())
