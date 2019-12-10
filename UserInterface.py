a788ab558cc81c15bb281bda779d0d95b3ce35e6
Arnaldur <aarnaldur@gmail.com>
2019-12-10 15:32:03 +0000

    Much progress on UI

--- UserInterface.py
+++ UserInterface.py
@@ -1,3 +1,6 @@
+from Destination import Destination
+from Employee import Employee
+from Airplane import Airplane
 from DestinationLL import DestinationLL
 from EmployeeLL import EmployeeLL
 from AirplaneLL import AirplaneLL
@@ -19,56 +22,6 @@ class State:
         self.title = title
         self.commands = commands
 
-
-class StateSpace:
-    def __init__(self):
-        self.current = 0
-        self.states = {
-            0:
-            State(
-                "Main Menu", {
-                    "1": Command("1", "Employees", 1),
-                    "2": Command("2", "Airplanes", 2),
-                    "3": Command("3", "Destinations", 3),
-                    "4": Command("4", "Voyages", 4),
-                    "q": Command("q", "Quit the program"),
-                }),
-            1:
-            State(
-                "Employees", {
-                    "c": Command(
-                        "c",
-                        "Create new employee",
-                        5,
-                    ),
-                    "s": Command("s", "Select sorting method", 6),
-                    "b": Command("b", "Back to main menu", 0),
-                }),
-            2:
-            State("Airplanes"),
-            3:
-            State("Destinations"),
-            4:
-            State("Voyages"),
-            5:
-            State("New "),
-        }
-
-    def title(self):
-        return self.states[self.current].title
-
-    def commands(self):
-        return self.states[self.current].commands
-
-    def transition(self, user_input):
-        command = self.states[self.current].commands.get(user_input, None)
-        if command == None:
-            pass  # punish the user
-        else:
-            self.current = command.transition
-        print("hwer ", command.transition)
-
-
 class Menu:
     def title(self):
         return ""
@@ -81,6 +34,9 @@ class Menu:
 
     def listing(self):
         return ""
+
+    def prompt(self):
+        return "Input Command: "
 
     def commands_printable(self):
         return "\n".join([str(e) for e in self.commands().values()])
@@ -111,86 +67,105 @@ class Asset(Menu):
     def __init__(self):
         self.HUI = HelperUI()
         self.asset_list = self.logic.get_all()
-        self.current_page = 0
+        self.page_length = 9
+        self.current_page = 1
+        self.page_count = 1 + (len(self.asset_list)//9)
 
     def commands(self, sorts=True):
         result = {
-            "c": Command("c", "Create new " + self.asset, CreationMenu(self)),
+            "c": Command("c", "Create new " + self.asset, EditingMenu(self)),
             "b": Command("b", "Back to main menu", MainMenu()),
         }
         if sorts:
             result["s"] = Command("s", "Select sorting method", 6)
         return result
 
+    def title(self):
+        return self.asset + " list"
+
     def has_list(self):
         return True
 
     def listing(self):
-        for i in range(9):
-            try:
-                print("  {}: {}".format(
-                    1 + i,
-                    self.asset_list[(self.current_page)+i].get_summary()))
-            except IndexError:
-                print("")
-        # self.HUI.list_screen(self.asset_list, self.title(), [], 1,1)
+        return "\n".join(["  {}: {}".format(1 + i, e.get_summary()) for i,e in enumerate(self.asset_list)])
+
+    def selected(self, user_input):
+        if user_input.isdigit():
+            return EditingMenu(self, self.asset_list[int(user_input) - 1])
+        else:
+            return super().selected(user_input)
 
 
 class EmployeeMenu(Asset):
     def __init__(self):
-        self.asset = "employee"
+        self.asset = "Employee"
         self.logic = EmployeeLL()
         super().__init__()
 
-    def title(self):
-        return "Employee list"
+    def new(self):
+        return Employee()
 
 
 class AirplaneMenu(Asset):
     def __init__(self):
-        self.asset = "airplane"
+        self.asset = "Airplane"
         self.logic = AirplaneLL()
         super().__init__()
 
-    def title(self):
-        return "Airplane list"
+    def new(self):
+        return Airplane()
 
 
 class DestinationMenu(Asset):
     def __init__(self):
-        self.asset = "destination"
+        self.asset = "Destination"
         self.logic = DestinationLL()
         super().__init__()
 
-    def title(self):
-        return "Destination list"
-
     def commands(self):
         return super().commands(True)
+
+    def new(self):
+        return Destination()
 
 
 class VoyageMenu(Asset):
     def __init__(self):
-        self.asset = "voyage"
+        self.asset = "Voyage"
         self.logic = EmployeeLL()
         super().__init__()
-
-    def title(self):
-        return "Voyage list"
 
     def commands(self):
         return super().commands(True)
 
 
-class CreationMenu(Menu):
-    def __init__(self, thing):
+class EditingMenu(Asset):
+    def __init__(self, thing, focused_asset=None):
         self.thing = thing
+        self.new_asset = thing.new() if focused_asset is None else focused_asset
+        self._title = "New" if focused_asset is None else "Update"
+        self.current_index = 0
 
     def title(self):
-        return "New " + self.thing.asset
+        return self._title + " " + self.thing.asset
 
     def commands(self):
-        return {}
+        result = {
+            "b": Command("b", "Back to " + self.thing.asset + " list", self.thing),
+        }
+        return result
+
+    def listing(self):
+        key_list = self.new_asset.get_keys()
+        header_list = self.new_asset.get_header()
+        arrow_pos = [
+            " <---" if self.current_index == pos else ""
+            for pos in range(len(header_list))
+        ]
+        return "\n".join(["{:>13}: {}{}".format(header, self.new_asset[key], arrow) for header, key, arrow in zip(header_list, key_list, arrow_pos)])
+
+    def prompt(self):
+        return "Enter {}: ".format(self.new_asset.get_header()[self.current_index])
 
 
 class UserInterface:
@@ -218,57 +193,9 @@ class UserInterface:
             print(self.menu.commands_printable())
             print(self.separator())
 
-            user_input = input("Input Command: ")
+            user_input = input(self.menu.prompt())
 
             self.menu = self.menu.selected(user_input)
             if user_input == "q":
-                return
+                return # we outa here
 
-    def old_loop(self, asset_name="Asset"):
-        asset_list = self.LL.get_all()
-
-        title = asset_name + " List"
-        instruction_list = [
-            "c. Create new " + asset_name, "b. Back to Main Menu"
-        ]
-        current_page = 1
-
-        num_pages = len(asset_list) // self.HUI.PAGE_DELIMETER
-        if len(asset_list) % self.HUI.PAGE_DELIMETER != 0:
-            num_pages += 1
-        if num_pages == 0:
-            num_pages += 1
-
-        while True:
-            self.HUI.list_screen(asset_list, title, instruction_list,
-                                 current_page, num_pages)
-
-            user_input = input("Enter command: ")
-
-            if user_input == "d":
-                if current_page == num_pages:
-                    current_page = 1
-                else:
-                    current_page += 1
-
-            elif user_input == "a":
-                if current_page == 1:
-                    current_page = num_pages
-                else:
-                    current_page -= 1
-
-            elif user_input.isdigit():
-                if int(user_input) >= 1 and int(
-                        user_input) <= self.HUI.PAGE_DELIMETER:
-                    try:
-                        self.display_asset(asset_list[(current_page - 1) *
-                                                      self.HUI.PAGE_DELIMETER +
-                                                      int(user_input) - 1])
-                    except:
-                        pass
-
-            elif user_input == "c":
-                return self.create_asset()
-
-            elif user_input == "b":
-                return
