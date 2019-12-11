5bf93664f7589c6bb66074d347940fe6d4fe2f4f
nei-tu <Richard19@ru.is>
2019-12-11 15:25:05 +0000

    added get_page to logiclayer class

--- LogicLayer.py
+++ LogicLayer.py
@@ -1,5 +1,10 @@
 class LogicLayer:
 
+	def get_page(self,asset_list,current_page,page_delimiter = 9):
+		return_value = asset_list[(current_page -1)*page_delimiter:current_page*page_delimiter]
+		return_value+= [""]*(page_delimiter-len(return_value))
+		return return_value
+
 	def get_all(self):
 		return self.IO.get_all()
 
