a7b1f37c09b0ef26495686a7eb0c9adfb5a67c88
nei-tu <Richard19@ru.is>
2019-12-12 15:38:47 +0000

    added the updatable fields function to airplane class

--- Airplane.py
+++ Airplane.py
@@ -21,6 +21,9 @@ class Airplane:
     def get_save_info(self):
         return [self.name, self.manufacturer, self.plane_type, self.seat_cap, self.time_table]
 
+    def get_updatable_fields(self):
+        return [0]
+
 
     def update_info(self, new_info_list):
         name, manufacturer, plane_type, seat_cap = new_info_list
