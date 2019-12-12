b4131a211705989743eb99dacdd2d76ead6daf03
Arnaldur <aarnaldur@gmail.com>
2019-12-12 22:58:39 +0000

    actually added the new asset

--- ModelFolder/Voyage.py
+++ ModelFolder/Voyage.py
@@ -1,6 +1,12 @@
 class Voyage:
-
-    def __init__(self, destination, departure_time, departure_return_time, airplane, pilot_list, attendant_list, seats_sold):
+    def __init__(self,
+                 destination="",
+                 departure_time="",
+                 departure_return_time="",
+                 airplane="",
+                 pilot_list=[],
+                 attendant_list=[],
+                 seats_sold=0):
         self.destination = destination
         self.departure_time = departure_time
         self.departure_return_time = departure_return_time
@@ -10,19 +16,30 @@ class Voyage:
         self.seats_sold = seats_sold
 
     def get_header(self):
-        return ["Destination", "Departure", "Arrival", "Airplane", "Pilots", "Attendants", "Seats Sold"]
+        return [
+            "Destination", "Departure", "Arrival", "Airplane", "Pilots",
+            "Attendants", "Seats Sold"
+        ]
 
     def get_print_info(self):
-        return [self.destination, self.departure_time, self.departure_return_time, self.airplane, self.pilot_list, self.attendant_list, self.seats_sold]
+        return [
+            self.destination, self.departure_time, self.departure_return_time,
+            self.airplane, self.pilot_list, self.attendant_list,
+            self.seats_sold
+        ]
 
     def get_save_info(self):
-        return [self.destination, self.departure_time, self.departure_return_time, self.airplane, self.pilot_list, self.attendant_list, self.seats_sold]
+        return [
+            self.destination, self.departure_time, self.departure_return_time,
+            self.airplane, self.pilot_list, self.attendant_list,
+            self.seats_sold
+        ]
 
     def get_creation_fields(self):
         return [0, 1, 2, 3, 4, 5, 6]
 
     def get_updatable_fields(self):
-        return [4,5,6]
+        return [4, 5, 6]
 
     def update_info(self, new_info_list):
         destination, departure_time, departure_return_time, airplane, pilot_list, attendant_list, seats_sold = new_info_list
@@ -34,20 +51,17 @@ class Voyage:
         self.attendant_list = attendant_list
         self.seats_sold = seats_sold
 
-
     # def __str__(self):
     #     string = ""
     #     string += "NA020 KEF --> {} 12/12/2019 23:20\n".format(self.destination.abrev)
     #     string += "     NA021 KEF <-- {} 12/13/2019 02:20".format(self.destination.abrev)
     #     return string
 
-
-    def __str__ (self):
-        total  = ""
-        total+= "Time of Departure: "+departure_time+"\n"
-        total+= "Time of return Departure: "+departure_return_time+"\n"
-        total+= "pilot Count: "+str(len(pilot_list))+"\n"
-        total+= "attendant Count"+str(len(attendant_list))+"\n"
-        total+= "seats sold: "+str(seats_sold)+"\n"
-
-
+    def __str__(self):
+        total = ""
+        total += "Time of Departure: " + departure_time + "\n"
+        total += "Time of return Departure: " + departure_return_time + "\n"
+        total += "pilot Count: " + str(len(pilot_list)) + "\n"
+        total += "attendant Count" + str(len(attendant_list)) + "\n"
+        total += "seats sold: " + str(seats_sold) + "\n"
+        return total
--- UILayer/UserInterface.py
+++ UILayer/UserInterface.py
@@ -220,7 +220,7 @@ class VoyageMenu(Asset):
 class EditingMenu(Menu):
     def __init__(self, mother):
         self.mother = mother
-        self.asset_fields = self.new_asset.get_print_info()
+        self.asset_fields = self.focused_asset.get_print_info()
         self.current_index = 0
         self.confirming = False
 
@@ -247,7 +247,7 @@ class EditingMenu(Menu):
         return self.editable_fields[self.current_index]
 
     def listing(self):
-        header_list = self.new_asset.get_header()
+        header_list = self.focused_asset.get_header()
         return "\n".join([
             "{:>13}: {}{}".format(header, info,
                                   " <---" if i == self._valid_index() else "")
@@ -257,18 +257,21 @@ class EditingMenu(Menu):
 
     def prompt(self):
         return "Enter {}: ".format(
-            self.new_asset.get_header()[self.current_index])
+            self.focused_asset.get_header()[self.current_index])
 
     def handle_input(self, user_input):
         command = self.commander().has(user_input)
         if command == None:
+            # self.mother.logic.is_valid_input(self._valid_index(), user_input)
             self.asset_fields[self._valid_index()] = user_input
             self._with_shifted_field(1)
             return self
         return command.invoke()
 
     def _confirm(self):
-        self.new_asset.update_info(self.asset_fields)
+        self.focused_asset.update_info(self.asset_fields)
+        self.mother.logic.add(self.focused_asset)
+        self.mother.logic.set_final_page()
         return self.mother
 
     def _with_shifted_field(self, change):
@@ -277,17 +280,17 @@ class EditingMenu(Menu):
 
 class UpdateMenu(EditingMenu):
     def __init__(self, mother, focused_asset):
-        self.new_asset = focused_asset
+        self.focused_asset = focused_asset
         self._title = "Update"
-        self.editable_fields = self.new_asset.get_updatable_fields()
+        self.editable_fields = self.focused_asset.get_updatable_fields()
         super().__init__(mother)
 
 
 class CreationMenu(EditingMenu):
     def __init__(self, mother):
-        self.new_asset = mother.new()
+        self.focused_asset = mother.new()
         self._title = "New"
-        self.editable_fields = self.new_asset.get_creation_fields()
+        self.editable_fields = self.focused_asset.get_creation_fields()
         super().__init__(mother)
 
 class SortingMenu(Menu):
