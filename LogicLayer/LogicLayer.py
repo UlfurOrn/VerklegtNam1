00e20510b6fd9f87221120d2a371e47fae43f40a
Arnaldur <aarnaldur@gmail.com>
2019-12-13 23:08:33 +0000

    employees are JSON

--- LogicLayer/LogicLayer.py
+++ LogicLayer/LogicLayer.py
@@ -26,7 +26,7 @@ class LogicLayer:
         page_to_print += [""] * (self.page_length - len(page_to_print))
         return page_to_print
 
-     def get_time_schedule(self, asset, time_start, time_end):
+    def get_time_schedule(self, asset, time_start, time_end):
         schedule = []
         for voyage in asset.time_table:
             voyage = self.IOAPI.get_voyage_by_id(voyage)
@@ -65,7 +65,8 @@ class LogicLayer:
         return False
 
     def in_use(self, asset):
-        return self.intersects_schedule(asset, datetime.now())
+        return self.is_busy_during_period(asset, datetime.now(), datetime.now())
+        # return self.intersects_schedule(asset, datetime.now())
 
     def check_time_table(self, time_table, departure_time, arrival_time):
         DEPARTURE = 0
@@ -83,25 +84,22 @@ class LogicLayer:
                     return True
         return False
 
-    def is_busy_during_period(self, asset, departure_str, arrival_str):
-        departure_time, arrival_time = (self.str_to_datetime(departure_str),
-                                        self.str_to_datetime(arrival_str))
+    def is_busy_during_period_str(self, asset, departure_str, arrival_str):
+        return (self.str_to_datetime(departure_str), self.str_to_datetime(arrival_str))
+
+    def is_busy_during_period(self, asset, departure_time, arrival_time):
         asset_time_table = asset.time_table
 
         time_table_to_check = []
-        if len(asset_time_table) >=1:
-            for voyage_id in asset_time_table:
-                print(asset.time_table)
-                if voyage_id is not "":
-                    voyage = self.IOAPI.get_voyage_by_id(voyage_id)
-                    time_table_to_check.append(
-                        self.text_to_datetime(voyage.departure_time,
-                                              voyage.return_time))
-                    if not self.check_time_table(time_table_to_check, departure_time,
-                                                 arrival_time):
-                        return voyage
+        for voyage_id in asset_time_table:
+            voyage = self.IOAPI.get_voyage_by_id(voyage_id)
+            time_table_to_check.append(
+                (self.str_to_datetime(voyage.departure_time),
+                 self.str_to_datetime(voyage.return_time)))
+            if not self.check_time_table(time_table_to_check, departure_time,
+                                         arrival_time):
+                return voyage
         return False
-
 
     def get_is_busy_and_free(self, asset_list, departure_str, arrival_str):
         departure_time, arrival_time = (self.str_to_datetime(departure_str),
