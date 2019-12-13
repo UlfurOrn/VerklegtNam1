293557ba75d3c6a8e3da9fec8e60c6d8ff5340bf
Arnaldur <aarnaldur@gmail.com>
2019-12-13 21:06:09 +0000

    lots of things

--- LogicLayer/DestinationLL.py
+++ LogicLayer/DestinationLL.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from DataLayer.IOAPI import IOAPI
 from ModelFolder.Destination import Destination
 from LogicLayer.LogicLayer import LogicLayer
@@ -28,49 +29,52 @@ class DestinationLL(LogicLayer):
     def get_input_specifacation(self, field_index):
         if field_index == 0:
             return "insert name of the new destination's country"
-
         elif field_index == 1:
-            return  "insert the name of the new destination's airport"
-
+            return "insert the name of the new destination's airport"
         elif field_index == 2:
             return "insert the abreviation for your airport"
         elif field_index == 3:
             return "insert the time it takes to fly to the new destination"
-
         elif field_index == 4:
             return "insert the distance from Iceland"
-
         elif field_index == 5:
-            return "insert the name of the new destination's contanct" 
-
+            return "insert the name of the new destination's contanct"
         elif field_index == 6:
             return "insert the phone number of the new destination's contanct"
-
 
     def get_input_type(self, field_index):
         return str
 
     def is_valid_input(self, field_index, new_input):
-        if field_index == 0: # Country
+        if field_index == 0:  # Country
             return self.is_only_letters(new_input) and new_input != ""
-
-        elif field_index == 1: # Airport
+        elif field_index == 1:  # Airport
             return self.is_only_letters(new_input) and new_input != ""
-
-        elif field_index == 2: # Airport Abbreviation
+        elif field_index == 2:  # Airport Abbreviation
             return self.is_unique_abreviation(new_input) and new_input != ""
-
-        elif field_index == 3: # Time
+        elif field_index == 3:  # Time
             return self.is_time_format(new_input) and new_input != ""
-
-        elif field_index == 4: # Distance
+        elif field_index == 4:  # Distance
             return new_input.isdigit() and new_input != ""
-
-        elif field_index == 5: # Contact name
+        elif field_index == 5:  # Contact name
             return self.is_only_letters(new_input)
-
-        elif field_index == 6: # Contact number
+        elif field_index == 6:  # Contact number
             return self.is_only_digits(new_input)
-
         else:
             return True
+
+    def set_sorting_method(self, sorting_method):
+        if sorting_method == DestinationSortingMethods.BY_COUNTRY:
+            self.asset_list.sort(key=lambda destination: destination.country)
+        elif sorting_method == DestinationSortingMethods.BY_FLIGHT_TIME:
+            self.asset_list.sort(
+                key=lambda destination: destination.flight_time)
+        elif sorting_method == DestinationSortingMethods.BY_FLIGHT_DISTANCE:
+            self.asset_list.sort(
+                key=lambda destination: destination.flight_dist)
+
+
+class DestinationSortingMethods(Enum):
+    BY_COUNTRY = 0
+    BY_FLIGHT_TIME = 1
+    BY_FLIGHT_DISTANCE = 2
--- LogicLayer/LogicLayer.py
+++ LogicLayer/LogicLayer.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from DataLayer.IOAPI import IOAPI
 
 
@@ -41,12 +41,25 @@ class LogicLayer:
     def change_page(self, change):
         self._current_page = (self._current_page + change) % self.total_pages
 
-    def get_asset_at(self, number):
-        return self.asset_list[self._current_page * self.page_length + number -
+    def get_asset_at(self, index):
+        return self.asset_list[self._current_page * self.page_length + index -
                                1]
 
     def set_final_page(self):
         self._current_page = self.total_pages - 1
+
+    def get_schedule(self, asset):
+        pass
+
+    def intersects_schedule(self, asset, time):
+        first, last = time if time is tuple else (time, time)
+        for (start, end) in self.get_schedule(asset):
+            if start < time and time < end:
+                return True
+        return False
+
+    def in_use(self, asset):
+        return self.intersects_schedule(asset, datetime.now())
 
     def check_time_table(self, time_table, departure_time, arrival_time):
         DEPARTURE = 0
@@ -62,45 +75,39 @@ class LogicLayer:
             if time_table[i + 1][DEPARTURE] > arrival_time:
                 if time_table[i][ARRIVAL] < departure_time:
                     return True
-                else:
-                    return False
+        return False
 
-    def text_to_datetime(self, departure_str, arrival_str):
-        departure_time = self.str_to_datetime(departure_str)
-        arrival_time = self.str_to_datetime(arrival_str)
-        return departure_time, arrival_time
-
-    def is_busy(self, asset, departure_str, arrival_str):
-        departure_time, arrival_time = self.text_to_datetime(
-            departure_str, arrival_str)
+    def is_busy_during_period(self, asset, departure_str, arrival_str):
+        departure_time, arrival_time = (self.str_to_datetime(departure_str),
+                                        self.str_to_datetime(arrival_str))
         asset_time_table = asset.time_table
 
         time_table_to_check = []
         for voyage_id in asset_time_table:
             voyage = self.IOAPI.get_voyage_by_id(voyage_id)
             time_table_to_check.append([
-                self.text_to_datetime(voyage.departure_time,
-                                      voyage.departure_return_time)
+                (self.str_to_datetime(voyage.departure_time),
+                 self.str_to_datetime(voyage.return_time))
             ])
             if not self.check_time_table(time_table_to_check, departure_time,
                                          arrival_time):
                 return voyage
 
     def get_is_busy_and_free(self, asset_list, departure_str, arrival_str):
-        departure_time, arrival_time = self.text_to_datetime(
-            departure_str, arrival_str)
+        departure_time, arrival_time = (self.str_to_datetime(departure_str),
+                                        self.str_to_datetime(arrival_str))
 
         busy = []
         free = []
 
         for asset in self.asset_list:
-            voyage_if_busy = self.is_busy(asset)
+            voyage_if_busy = self.is_busy_during_period(asset)
             if asset_busy is not None:
-                busy.append(asset,asset_busy)
+                busy.append(asset, asset_busy)
             else:
                 free.append(asset)
 
-        return busy,free
+        return busy, free
 
     def is_only_letters(self, string):
         for c in string.strip():
@@ -115,18 +122,17 @@ class LogicLayer:
 
     def is_datetime_format(self, string):
         try:
-            datetime.datetime.fromisoformat(string)
+            datetime.fromisoformat(string)
             return True
         except:
-        	pass
-
+            return False
 
     def str_to_datetime(self, string):
-        return datetime.datetime.fromisoformat(string)
+        return datetime.fromisoformat(string)
 
     def str_to_time_delta(self, string):
         hour, minute = string.split(":")
-        datetime.timedelta(hour=int(hour),minute=int(minute))
+        datetime.timedelta(hour=int(hour), minute=int(minute))
 
     def get_all(self):
         return self.IOAPI.load()
